8a6f2128b689d80c713bfada94c98c531407e0dd
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2025-05-07 16:42:42 +0200

    Update old_config_to_new.sh

--- script/old_config_to_new.sh
+++ script/old_config_to_new.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sed -i '3,13d' /home/phablet/.config/radio.s710/radio.s710.conf
+file="/home/phablet/.config/radio.s710/already_migrated"
+
+if [ ! -f $file ]; then
+    sed -i '3,13d' /home/phablet/.config/radio.s710/radio.s710.conf
+    touch /home/phablet/.config/radio.s710/already_migrated
+fi
