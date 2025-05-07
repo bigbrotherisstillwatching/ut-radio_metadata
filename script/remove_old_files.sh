0bf546d060ea284bb9c0eb63da9f3b4bcd4d4e46
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2025-05-07 17:05:12 +0200

    remove file

--- script/remove_old_files.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-file="/home/phablet/.config/radio.s710/radio.s710.defaultsink.txt"
-file2="/home/phablet/.config/radio.s710/radio.s710.oldsink.txt"
-
-[ -f $file ] && rm -f $file
-[ -f $file2 ] && rm -f $file2
