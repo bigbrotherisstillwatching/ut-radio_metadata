71b0a8ab88047b5a381618727e20d087ba6d6c2c
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2025-05-07 16:34:02 +0200

    Update old_config_to_new.sh

--- script/old_config_to_new.sh
+++ script/old_config_to_new.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-var1=$(grep -m 1 "darkMode" /home/phablet/.config/radio.s710/radio.s710.conf)
-
-var2=$(grep -m 1 "favouriteStations" /home/phablet/.config/radio.s710/radio.s710.conf)
-
-var3=$(grep -m 1 "lastStation" /home/phablet/.config/radio.s710/radio.s710.conf)
-
-file="/home/phablet/.config/radio.s710/radio.s710.conf"
-
-sed -i "/darkMode/c $(printf %q "$var1")" /home/phablet/.config/radio.s710/radio.s710.new.conf
-
-sed -i "/favouriteStations/c $(printf %q "$var2")" /home/phablet/.config/radio.s710/radio.s710.new.conf
-
-sed -i "/lastStation/c $(printf %q "$var3")" /home/phablet/.config/radio.s710/radio.s710.new.conf
-
-sleep 2
-
-[ -f $file ] && rm -f $file
+sed -i '3,13d' /home/phablet/.config/radio.s710/radio.s710.conf
