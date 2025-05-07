d2535f2fb3ca22abccc68fca982536365784098c
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2025-05-07 23:06:59 +0200

    Update migrate_to_new_version.sh

--- script/migrate_to_new_version.sh
+++ script/migrate_to_new_version.sh
@@ -3,9 +3,10 @@
 file="/home/phablet/.config/radio.s710/already_migrated"
 file2="/home/phablet/.config/radio.s710/radio.s710.defaultsink.txt"
 file3="/home/phablet/.config/radio.s710/radio.s710.oldsink.txt"
+file4="/home/phablet/.config/radio.s710/radio.s710.conf"
 
 if [ ! -f $file ]; then
-    sed -i '3,13d' /home/phablet/.config/radio.s710/radio.s710.conf
+    sed -i '3,13d' $file4
     rm -f $file2
     rm -f $file3
     touch $file
